d6fa073c77e95b1ddf13c1fad08a3c5c8e4039cf
pablomm <pablo.marcosm@estudiante.uam.es>
2018-10-05 11:26:33 +0200

    Integrales montecarlo

--- Semana3/Integracion-Montecarlo.R
+++ Semana3/Integracion-Montecarlo.R
@@ -1,5 +1,6 @@
 
 # Semana 3 - Simulacion de montecarlo
+# Grupo 1 - Luis Jariego, Pablo Marcos, Roberto Alcover y Santiago Cerezo
 
 montecarlo <- function(f, n=10000,xlim=c(0,1), ylim=c(0,1), plt=F) {
   # Integra la funcion f en el intervalo xlim usando el metodo de 
@@ -72,10 +73,61 @@ print(integra(f,n)) # Metodo 2
 # Comparamos la variabilidad de cada uno de los 2 metodos
 k <- 100 # Numero de simulaciones de la integral
 
-metodo1 <- replicate(k, montecarlo(f,n))
+metodo1 <- replicate(k, montecarlo(f,n)   )
 metodo2 <- replicate(k, integra(f,n))
+metodo <- replicate(100, runif)
+
 
 par(mfrow=c(1,2))
+print("Comparacion metodo montecarlo vs naive montecarlo, desviaciones:")
+print(sd(metodo1))
+print(sd(metodo2))
 hist(metodo1, prob=T)
 hist(metodo2, prob=T)
+
+# Integramos el coseno pero calculando la esperanza de la uniforme y la beta
+alfa <- 2
+beta <- 2
+rf <- function(n) { rbeta(n, shape1 = alfa, shape2 = beta)}
+df <- function(x) { dbeta(x, shape1 = alfa, shape2 = beta)}
+
+f <- function(x) { sin(x*pi)}
+
+
+# Pintamos la funcion de densidad beta y la funcion a integrar
+xfit = seq(0,1,0.01)
+
+par(mfrow=c(1,1))
+plot(xfit, sapply(xfit, f), type="l", lwd=2, ylim=c(0,2))
+lines(xfit, sapply(xfit, df), lwd=2, col="red")
+legend("topright", legend=c("sin(x*pi)", paste("beta(",alfa,",",beta,")")),
+       col=c("black", "red"),lty=c(1,1))
+
+par(mfrow=c(1,2))
+metodo2_uniforme <- replicate(k, integra(f, n))
+metodo2_beta <- replicate(k, integra(f, n, rf=rf,df=df))
+
+print("Desviaciones estandar con uniforme vs beta")
+sd1 <- sd(metodo2_uniforme)
+sd2 <- sd(metodo2_beta)
+hist(metodo2_uniforme, prob=T, main=paste("Unif sd=",sd1))
+hist(metodo2_beta, prob=T, main=paste("Beta sd=",sd2))
+
+# Veamos como afecta a la desviacion el numero de muestras
+desviaciones_beta <- c()
+desviaciones_unif <- c()
+i <- 1
+sims = c(100,500,1000, 5000, 10000)
+for (n in sims){
+  metodo2_beta <- replicate(k, integra(f, n, rf=rf,df=df))
+  metodo2_uniforme <- replicate(k, integra(f, n))
+  desviaciones_beta[[i]] <- sd(metodo2_beta)
+  desviaciones_unif[[i]] <- sd(metodo2_uniforme)
+  i <- i+1
+}
+par(mfrow=c(1,1))
+xfit = seq(0, 1e4,length=100)
+#plot(xfit, 1/sqrt(xfit), type="l", col="red", lwd=2, main="Comparacion convergencia")
+plot(desviaciones_unif, desviaciones_beta, type="o", main="Comparacion convergencia", lwd=2)
+plot(sims, desviaciones_beta/desviaciones_unif, type="o", main="Comparacion convergencia sd_beta/sd_unif", lwd=2)
 
